07ad518fc2559f439457cdea8f41949398b64494
Bram Doppen <bramdoppen@gmail.com>
2023-11-15 00:26:12 +0100

    Adding gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["nuxt-gtag"],
+  gtag: {
+    id: "G-Q74C09JKR2",
+  },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
